8204e47f41b5ef9cd4fa5847fa5acd0e76b71b2b
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2025-01-03 21:03:40 +0100

    Update .config/home-manager/home.nix

--- private_dot_config/home-manager/create_home.nix
+++ private_dot_config/home-manager/create_home.nix
@@ -67,9 +67,7 @@
       set fish_greeting # Disable greeting
       if command -q nix-your-shell
         nix-your-shell fish | source
-      end
-      set pure_enable_nixdevshell true
-      set pure_symbol_nixdevshell_prefix 󱄅 '';
+      end'';
     plugins = [
       { name = "fzf"; src = pkgs.fishPlugins.fzf-fish.src; }
       { name = "pure"; src = pkgs.fishPlugins.pure.src; }
