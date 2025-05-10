09b79ad3b03f111034a370fd615941c99927b22a
Côme VINCENT <44554692+comejv@users.noreply.github.com>
2025-05-10 16:24:39 -0400

    Update .config/nvim/lua/plugins/init.lua

--- private_dot_config/nvim/lua/plugins/create_init.lua
+++ private_dot_config/nvim/lua/plugins/create_init.lua
@@ -45,4 +45,11 @@ return {
       { "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
     },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 }
